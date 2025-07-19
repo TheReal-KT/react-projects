110944b59f57d5922ddd4b55d6ca7c183449e213
Khuluza Tshabalala <khuluza0@gmail.com>
2025-07-19 19:25:11 +0200

    Fixed image width

--- Travel Journal Application/components/entry.jsx
+++ Travel Journal Application/components/entry.jsx
@@ -1,14 +1,13 @@
 function Entry(props) { 
     return ( 
         <article className="flex items-center justify-center gap-4 py-4">
-            <div>
-                <img src= {props.img} className="rounded-lg max-1-100"/>
+            <div className="w-48 flex-shrink-0">
+                <img src={props.img} alt={props.title} className="w-full h-48 object-cover rounded-lg shadow-md"/>
             </div>
             <div className="flex flex-col text-left ml-4 font-[inter] gap-2.5">
                 <span className="flex flex-row gap-2.5">
                     <b>{props.location}</b>
-                    <a href= {props.link} target="_blank"><p>View on Google</p>
-                    </a>
+                    <a href={props.link} target="_blank" className="text-gray-500 underline">View on Google</a>
                 </span>
                 <h1>{props.title}</h1>
                 <p><b>{props.date}</b></p>
