41adef800d4d6717ded2857d1fb3796dfbda60a0
Khuluza Tshabalala <khuluza0@gmail.com>
2025-07-20 17:11:44 +0200

    Added glassmorphism effect
    Added the navigation bar information within the header

--- Travel Journal Application/components/header.jsx
+++ Travel Journal Application/components/header.jsx
@@ -2,9 +2,16 @@ import globe from './assets/Globe.png'
 
 function Header() { 
     return ( 
-        <div className='w-full flex bg-red-400 items-center justify-center py-3 fixed top-0 left-0'>
-            <img src={globe} alt="globe-logo" />
-            <h1 className="ml-4 !text-lg font-bold font-[inter] text-white ">Travel Journal</h1>
+        <div className='fixed top-5 inset-x-5 flex items-center justify-center rounded-xl py-3 shadow-2xl border-white/20 backdrop-blur-lg'>
+            <nav className='flex items-center'> 
+                <img src={globe} alt="globe-logo" />
+                <h1 className="ml-4 !text-lg font-bold font-[inter] text-black ">Travel Journal</h1>
+                <ul className='flex items-center gap-2 p-4 font-[inter] text-black'>
+                    <li>About</li>
+                    <li>Portfolio</li>
+                    <li>Docs</li>
+                </ul>
+            </nav>
         </div>
     )
 }
